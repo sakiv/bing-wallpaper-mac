a2151c2779c371c3802118c5d81971ac7ffaef9b
Vikas Aggarwal <vikas.aggarwal@synpulse.com>
2023-11-16 18:37:42 -0500

    Added new feature to collect wallpapers.

--- bing-wallpaper.sh
+++ bing-wallpaper.sh
@@ -4,12 +4,6 @@ PICTURE_DIR="$HOME/Pictures/Bing-Wallpapers"
  
 mkdir -p $PICTURE_DIR
 
-# Picks
-# urls=( $(curl -sSL http://www.bing.com | \
-#     grep -Eo "url:'.*?'" | \
-#     sed -e "s/url:'\([^']*\)'.*/http:\/\/bing.com\1/" | \
-#     sed -e "s/\\\//g") )
-
 urls=( $(curl -sSL https://www.bing.com | \
     grep -Eo 'g_img={url:\s*".*?"' | \
 	sed -e 's/g_img={url:\s*"//g' | \
@@ -60,25 +54,37 @@ then
         urls="http://www.bing.com$urls"
     fi
  
+    today=$(date +%Y%m%d)
+    file_ext=$(echo $urls|sed -e "s/.*\/\(.*\)/\1/"|sed -e "s/.*\.//g")
+
+    # # Remove files older than 6 days
+    # find $PICTURE_DIR/*.$file_ext -type f -mtime +6 -print0 | xargs -r0 echo --
+
+    Remove all files in the directory
     rm -Rf $PICTURE_DIR/*.*
  
-#   Use this for today's Bing Wallpaper 
-    filename=$(echo $urls|sed -e "s/.*\/\(.*\)/\1/")
-    # echo $urls
-    # exit 0
-    curl -Lo "$PICTURE_DIR/$filename" $urls  
+    # Use this for today's Bing Wallpaper 
+    filename="$PICTURE_DIR/bing-wallpaper.$file_ext"
+
+    # Use this for every days file with timestamp
+    # filename="$PICTURE_DIR/bing-wallpaper-$today.$file_ext"
+    
+    # Download the file
+    echo "Downloading: $filename ..."
+    curl -sSLo $filename $urls  
          
-#   Use this for today's and next day's Bing Wallpaper 
-#       
-#   for p in ${urls[@]}; do
-#       filename=$(echo $p|sed -e "s/.*\/\(.*\)/\1/")
-#       if [ ! -f $PICTURE_DIR/$filename ]; then
-#           echo "Downloading: $filename ..."
-#           curl -sSLo "$PICTURE_DIR/$filename" $p
-#       else
-#           echo "Skipping: $filename ..."
-#       fi
-#   done
+    # # Use this for today's and next day's Bing Wallpaper -- Not working anymore
+      
+    # for p in ${urls[@]}; do
+    #     filename=$(echo $p|sed -e "s/.*\/\(.*\)/\1/")
+    #     if [ ! -f $PICTURE_DIR/$filename ]; then
+    #         echo "Downloading: $filename ..."
+    #         file_ext = $filename | sed -e "s/.*\.//g"
+    #         curl -sSLo "$PICTURE_DIR/bing-wallpaper-$today.$file_ext" $p
+    #     else
+    #         echo "Skipping: $filename ..."
+    #     fi
+    # done
  
     killall Dock
     exit 0
